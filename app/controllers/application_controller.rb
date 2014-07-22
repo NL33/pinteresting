f3906e8214c41f8cd8dc0da91084ae7713ea531a
Sean Kellman <stkellman@gmail.com>
2014-07-22 14:46:18 -0400

    added use of user name and requirement of entering and having user name show up with pin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?  # This creates a before action, saying configure permitted paramaters, and only if the devise controller is the controller in play. Devise controls stuff about users edit their account, login, etc… So this kicks in only if one of these is in play.]]
+
+protected # this means only the application can see this
+
+	def configure_permitted_parameters #this is copied from the gitub/devise description
+  		devise_parameter_sanitizer.for(:sign_up) << :name #this is for signup   
+   		devise_parameter_sanitizer.for(:account_update) << :name #his is for the account update page
+	end
+
 end
+
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,8 @@ class Pin < ActiveRecord::Base
      
      has_attached_file :image, :styles => { :large => "800x800!", :medium => "575x625#", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
      validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
+     validates :image, presence: true #added to ensure a user uploads an image and description
+	 validates :description, presence: true
+
 end
